80d7f7779a3bac5ae1b4e28c1c32d560024bc1f9
mwdeshaw <mwdeshaw@gmail.com>
2019-06-06 16:28:05 -0700

    Create UA system and bands

--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -30,25 +30,31 @@ class User < ApplicationRecord
 
     def password=(password)
         @password = password
-        self.password_digest = BCrypt.Password.create(password)
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def is_password?(password)
-        BCrypt.Password.new(self.password_digest).is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
     def reset_session_token!
-        self.session_token = SecureRandom.urlsafe_base64
+        self.session_token = generate_unique_session_token
         self.save!
         self.session_token
     end
 
-    def self.generate_session_token
-        self.session_token = SecureRandom.urlsafe_base64
+  def generate_unique_session_token
+    token = SecureRandom.urlsafe_base64(16)
+
+    while self.class.exists?(session_token: token)
+      token = SecureRandom.urlsafe_base64(16)
     end
+
+    token
+  end
 
     private
     def ensure_session_token
-        self.session_token ||= SecureRandom.urlsafe_base64
+        self.session_token ||= generate_unique_session_token
     end
 end
--- MusicApp/db/schema.rb
+++ MusicApp/db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_170704) do
+ActiveRecord::Schema.define(version: 2019_06_06_203521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bands", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "users", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
